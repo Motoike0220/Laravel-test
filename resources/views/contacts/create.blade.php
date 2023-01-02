75beea7ccc99115f6270309cf9d50ba73ea93667
Motoike0220 <104264544+Motoike0220@users.noreply.github.com>
2023-01-02 21:25:16 +0900

    フォーム登録

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -47,8 +47,8 @@
                                 <div class="p-2 w-full">
                                     <div class="relative">
                                         <label class="leading-7 text-sm text-gray-600">性別</label>
-                                        <input type="radio" name="gemder" value="0">男性
-                                        <input type="radio" name="gemder" value="1">女性
+                                        <input type="radio" name="gender" value="0">男性
+                                        <input type="radio" name="gender" value="1">女性
                                     </div>
                                 </div>
 
@@ -71,8 +71,8 @@
 
                                 <div class="p-2 w-full">
                                     <div class="relative">
-                                        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-                                        <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                        <label for="contact" class="leading-7 text-sm text-gray-600">Message</label>
+                                        <textarea id="contact" name="contact" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                     </div>
                                 </div>
 
